2d737ce942747e5e7095f575a34e8ffbc5a86d64
Zach Nusbaum <zach@zdnmail.com>
2019-06-04 21:47:11 -0700

    add permits

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { PlaceholderModule } from './placeholder/placeholder.module';
 import { CatsModule } from './cats/cats.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PermitsModule } from './permits/permits.module';
 
 @Module({
   imports: [
     PlaceholderModule,
     CatsModule,
     MongooseModule.forRoot(process.env.MONGO_URL || 'mongodb://localhost/nest'),
+    PermitsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
